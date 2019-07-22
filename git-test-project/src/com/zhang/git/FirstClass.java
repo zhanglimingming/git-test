2efe89494869ba7bd1fbc0d9a746b0713baf5388
zhangliming <1491272775@qq.com>
2019-07-22 22:03:29 +0800

    修改FirstClass文件

--- git-test-project/src/com/zhang/git/FirstClass.java
+++ git-test-project/src/com/zhang/git/FirstClass.java
@@ -2,6 +2,7 @@ package com.zhang.git;
 
 public class FirstClass {
     public static void main(String[] args) {
+        //hello test
         System.out.println("hello git project");
         System.out.println("hello git project");
         System.out.println("hello git project");
@@ -12,5 +13,9 @@ public class FirstClass {
     }
     public void test(){
         System.out.println("测试一下");
+
+        System.out.println("yi");
+        System.out.println("ci");
+        System.out.println("jiu");
     }
 }
